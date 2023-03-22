d82cfe4d5759cbea6b68dc8cc569b53c617b7b6a
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-03-22 23:49:11 +0200

    feat(Lab2): Parse news name and link

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,9 +1,53 @@
 const fs = require('fs');
 const net = require('net');
 const http = require('http');
+const https = require('https');
+const cheerio = require('cheerio');
 
 const HOST = 'localhost';
 const PORT = 8080;
+const options = {
+  host: "www.overclockers.ua",
+  port: 443,
+  path: "/"
+};
+
+const workdir = "./news";
+
+var source = "";
+
+const loadAndParse = async function () {
+  console.log("Parser started");
+  
+  source = "";
+  
+  var req = https.request(options, (response) => {
+    response.on("data", (chunk) => source += chunk);
+    response.on("end", () => {
+      const $ = cheerio.load(source);
+      const selector = 
+         "#overblock > #main > #content > #mainpage > div.post";
+    
+      $(selector).each((parentIndex, parentElement) => {
+        $(parentElement)
+          .children("div.post_desc")
+          .children("h2")
+          .children((childId, childElem) => {
+            const news_name = $(childElem).text();
+            const path = $(childElem).attr('href');
+            
+            // Create directory if not exists
+            if (!fs.existsSync(workdir)) {
+              fs.mkdirSync(workdir);
+            }
+
+          });
+      });
+    });
+  });
+  req.end();
+  console.log("Parser ended");
+};
 
 // Timer, to perform refresh of information every 60 seconds
 (async function() {
@@ -12,6 +56,7 @@ const PORT = 8080;
   while(true) {
     if (Date.now() - timer > 60000) {
       console.log("60 seconds passed, refreshing news...");
+      await loadAndParse();
       timer = Date.now();
     }
     await sleep(1000);
@@ -30,8 +75,9 @@ const requestListener = function (request, response) {
   console.log(`Request: ${request.method} ${request.url} was at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
   switch (request.url) {
     case "/api/news":
+      response.setHeader("Content-Type", "text/html");
       response.writeHead(200);
-      response.write("News web parser.\n");
+      response.write(source);
       response.end();
       break;
     default:
@@ -44,6 +90,7 @@ const requestListener = function (request, response) {
 
 
 const app = http.createServer(requestListener);
+loadAndParse();
 app.listen(PORT, HOST, () => {
   console.log(`App is running on http://${HOST}:${PORT}`);
 });
